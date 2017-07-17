c15ea8fd68983264a24a038a18d63ee6ac866d58
Itte Forsberg <paalkn@imac.local>
2017-07-17 09:32:33 +0200

    Initial Commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Pål Knudsen on 17.07.2017.
+//  Copyright © 2017 Pål Knudsen. All rights reserved.
+//
+
+import Foundation
+
+print("Detta er fett!")
+
